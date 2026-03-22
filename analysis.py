3fbb0de75543e901d352267d79da36ffae6ae2b6
vanshikakyatam <vanshikakyatam04@gmail.com>
2026-03-22 17:08:51 +0530

    Initial commit - Netflix Data Analysis

---
+++ analysis.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("netflix_titles.csv")
+print(df.head())
+print(df.columns)
+
+# Movies vs TV Shows
+type_count = df['type'].value_counts()
+
+type_count.plot(kind='bar')
+plt.title("Movies vs TV Shows")
+plt.xlabel("Type")
+plt.ylabel("Count")
+plt.show()
+
+# Top Countries
+df['country'] = df['country'].fillna("Unknown")
+
+top_countries = df['country'].value_counts().head()
+
+top_countries.plot(kind='bar')
+plt.title("Top 5 Content Producing Countries")
+plt.xlabel("Country")
+plt.ylabel("Count")
+plt.show()
+
+# Content Trend Over Years
+df['date_added'] = pd.to_datetime(df['date_added'], errors='coerce')
+
+yearly_content = df['date_added'].dt.year.value_counts().sort_index()
+
+yearly_content.plot(kind='line')
+plt.title("Content Added Over Years")
+plt.xlabel("Year")
+plt.ylabel("Number of Shows")
+plt.show()
+
+# Rating Analysis
+df['rating'].value_counts().plot(kind='bar')
+plt.title("Content Ratings Distribution")
+plt.xlabel("Rating")
+plt.ylabel("Count")
+plt.show()
